1fb9fa7acf62e65f9c98a5ec159242ac8168bdbf
James Hoffman <james-hoffman@live.ca>
2026-04-30 11:31:53 -0400

    add vue-router, tweaks dev vs release

--- src/app/app.js
+++ src/app/app.js
@@ -8,39 +8,47 @@ export default {
   },
 
   template: `
-      <v-app>
-          <v-layout>
-      <v-navigation-drawer
-        v-model="drawer"
-        :rail="true"
-        color="indigo"
-        permanent
+    <v-app>
+
+      <div 
+        v-if="!!dev" 
+        style="background-color: red; color: white; padding: 4px; text-align: center;"
       >
-        <v-list density="compact" nav>
-          <v-list-item
-            prepend-icon="mdi-history"
-            title="History"
-            value="1"
-          ></v-list-item>
+        [dev]
+      </div>
 
-          <v-list-item
-            prepend-icon="mdi-cog"
-            title="Settings"
-            value="1"
-          ></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <v-layout>
+        <v-navigation-drawer
+          v-model="drawer"
+          :rail="true"
+          color="indigo"
+          permanent
+        >
+          <v-list density="compact" nav>
+            <v-list-item
+              :active="true"
+              prepend-icon="mdi-history"
+              title="History"
+              value="1"
+            ></v-list-item>
 
-      <v-navigation-drawer :permanent="true">
-        <h1>nav drawer</h1>
-      </v-navigation-drawer>
+            <v-list-item
+              prepend-icon="mdi-cog"
+              title="Settings"
+              value="1"
+            ></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
 
-      <v-main style="height: 250px">
-       <h1>main</h1>
-       {{ (!!dev).toString() }}
-      </v-main>
-    </v-layout>
-      </v-app>
+        <v-navigation-drawer :permanent="true">
+          <h1>nav drawer</h1>
+        </v-navigation-drawer>
+
+        <v-main>
+          <router-view />
+        </v-main>
+      </v-layout>
+    </v-app>
   `,
 
   async mounted() {
@@ -50,11 +58,13 @@ export default {
       Neutralino.app.exit();
     });
 
-    Neutralino.app.getConfig().then( c => {
-      // this.dev = c.devMode
-    })
-
     this.dev = await Neutralino.os.getEnv('DEV');
+
+    if (!this.dev) {
+      window.addEventListener('contextmenu', (event) => {
+        event.preventDefault()
+      })
+    }
   }
 
 }
