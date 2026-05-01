b2a5a488061726d84b7979cd3dff2054d1a61ced
James Hoffman <james-hoffman@live.ca>
2026-05-01 14:04:31 -0400

    PoC curl extension

---
+++ extensions/curl/curl.js
@@ -0,0 +1,196 @@
+// Source
+// https://github.com/hschneider/neutralino-curl/blob/master/README.md
+// (c)2024 Harald Schneider - marketmix.com
+
+export class Curl {
+    constructor(opt= {}) {
+        //
+        // Constructor
+
+        this.version = '1.1.0';
+        this.debug = opt.debug || false;
+
+        this.progress = 0;  // Current progress
+        this.httpHeaders = [];  // List of HTTP headers
+
+        // NL_RESMODE
+        const bin = `nl://src/vendor/curl/${NL_OS}-${NL_ARCH}`.toLowerCase()
+        this.appResourcesEXT = NL_PATH + bin
+        if(NL_OS === 'Windows' ) {
+            this.appResourcesEXT = NL_CWD + bin
+        }
+
+        // wip
+        this.appResourcesEXT = bin
+
+        // Auth credentials
+        //
+        this.auth = {
+            usr: '',
+            pwd: ''
+        }
+    }
+
+    setCredentials(usr, pwd) {
+        //
+        // Set credentials for e.g. FTP
+
+        this.auth.usr = usr;
+        this.auth.pwd = pwd;
+    }
+    addHttpHeader(key, value) {
+        //
+        // Add a custom http-header
+
+        let h = key + ": " + value;
+        this.httpHeaders.push(h);
+    }
+    clearHttpHeader() {
+        //
+        // Clears http-header list.
+
+        this.httpHeaders = [];
+    }
+
+    async get(url) {
+        let httpHeader = '';
+        if(this.httpHeaders.length > 0) {
+            this.httpHeaders.map(h => {
+                httpHeader += `-H "${h}" `;
+            });
+            console.log(httpHeader);
+        }
+        return await this.run(`-k ${httpHeader} ` + url);
+    }
+
+    async post(url, data) {
+        let httpHeader = '';
+        if(this.httpHeaders.length > 0) {
+            this.httpHeaders.map(h => {
+                httpHeader += `-H "${h}" `;
+            });
+            console.log(httpHeader);
+        }
+        let d = JSON.stringify(data);
+        return await this.run(`-k ${httpHeader} --request POST ${url} -d ${d}`);
+    }
+
+    async download(src, dst="") {
+        //
+        // Download via http, ftp or ftps
+
+        let auth = this.auth.usr+":"+this.auth.pwd;
+
+        let ftpSSL = '';
+        if(src.includes('ftps://')) {
+            ftpSSL = '--ftp-ssl'
+        }
+
+        let httpHeader = '';
+        if(this.httpHeaders.length > 0) {
+            this.httpHeaders.map(h => {
+                httpHeader += `-H "${h}" `;
+            });
+            console.log(httpHeader);
+        }
+
+        if(src.includes('http://') || src.includes('https://')) {
+            if(dst === '') {
+                await this.run(`--progress-bar ${httpHeader} -L -k -O ${src}`);
+            }
+            else {
+                // Download as
+                await this.run(`--progress-bar ${httpHeader} -L -k -o ${dst} ${src}`);
+            }
+        }
+
+        if(src.includes('ftp://') || src.includes('ftps://')) {
+            if(dst === '') {
+                await this.run(`--progress-bar -k ${ftpSSL} -u ${auth} -O ${src}`);
+            }
+            else {
+                // Download as
+                await this.run(`--progress-bar -k ${ftpSSL} -u ${auth} -o ${dst} ${src}`);
+            }
+        }
+    }
+    async upload(src, dst) {
+        //
+        // Upload via http, ftp or ftps
+
+        let auth = this.auth.usr+":"+this.auth.pwd;
+
+        let ftpSSL = '';
+        if(src.includes('ftps://')) {
+            ftpSSL = '--ftp-ssl'
+        }
+
+        let httpHeader = '';
+        if(this.httpHeaders.length > 0) {
+            this.httpHeaders.map(h => {
+                httpHeader += `-H "${h}" `;
+            });
+            console.log(httpHeader);
+        }
+
+        if(dst.includes('http://') || dst.includes('https://')) {
+            await this.run(`--progress-bar -k ${httpHeader} -F file=@${src} ${dst}`);
+        }
+        if(dst.includes('ftp://') || dst.includes('ftps://')) {
+            await this.run(`--progress-bar -k ${ftpSSL} -u ${auth} -T ${src} ${dst}`);
+        }
+    }
+    async run(args) {
+        //
+        // Run curl with any argument
+
+        let eStart = new Event("curlStart");
+        document.dispatchEvent(eStart);
+
+        alert(`${this.appResourcesEXT}/bin/curl ${args}`)
+        // let cmd = await Neutralino.os.spawnProcess(`${this.appResourcesEXT}/bin/curl ${args}`);
+        let cmd = await Neutralino.os.spawnProcess(`ls ./`);
+
+        Neutralino.events.on('spawnedProcess', (e) => {
+            if(cmd.id == e.detail.id) {
+              alert(e.detail.data)
+                switch(e.detail.action) {
+                    case 'stdOut':
+                        let eData = new CustomEvent("curlData", {detail: e.detail.data});
+                        document.dispatchEvent(eData);
+                        break;
+                    case 'stdErr':
+                        const m = e.detail.data.match(/\d+\.\d+/);
+                        if( m !== null && parseFloat(m[0]) >= this.progress) {
+                            this.progress = parseFloat(m[0]);
+                            if(this.debug) {
+                                console.log("Curl progress in percent: "+m[0]);
+                            }
+                            let eProgress = new CustomEvent("curlProgress", {detail: Math.round(parseFloat(m[0]))});
+                            document.dispatchEvent(eProgress);
+                        }
+                        break;
+                    case 'exit':
+                        if(this.debug) {
+                            console.log(`Curl terminated with exit code: ${e.detail.data}`);
+                        }
+                        if(e.detail.data == 0) {
+                            let eProgress = new CustomEvent("curlProgress", {detail: 100.0});
+                            document.dispatchEvent(eProgress);
+                        }
+                        let eEnd = new CustomEvent("curlEnd", {detail: parseInt(e.detail.data)});
+                        document.dispatchEvent(eEnd);
+                        break;
+                }
+            }
+        });
+    }
+    async resetProgress() {
+        //
+        // Reset progress-counter and emit curlProgress event.
+        
+        this.progress= 0.0;
+        let eProgress = new CustomEvent("curlProgress", {detail: 0.0});
+        document.dispatchEvent(eProgress);
+    }
+}
--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,5 @@
 import Panel from '/app/nav/panel.js'
+import { Curl } from '/vendor/curl.js'
 
 export default {
   components: {
@@ -32,19 +33,26 @@ export default {
   `,
 
   async mounted() {
-    Neutralino.init();
+    Neutralino.init()
+    const curl = new Curl()
 
     Neutralino.events.on("windowClose", () => {
-      Neutralino.app.exit();
+      Neutralino.app.exit()
     });
 
-    this.dev = await Neutralino.os.getEnv('DEV');
+    this.dev = await Neutralino.os.getEnv('DEV')
 
     if (!this.dev) {
       window.addEventListener('contextmenu', (event) => {
         event.preventDefault()
       })
     }
+
+    let result = await curl.get('https://httpbin.org/base64/SFRUUEJJTiBpcyBhd2Vzb21l');
+    console.log(result)
+    document.addEventListener('curlData', (e) => {
+      alert(e.detail)
+    })
   }
 
 }
