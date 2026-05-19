397ef83e2d8fc1651aa9652cc42fe8d22c28643e
James Hoffman <james-hoffman@live.ca>
2026-05-19 18:58:37 -0400

    v0.1.2 fix translation reactivity

--- src/app/translate/translate.service.js
+++ src/app/translate/translate.service.js
@@ -6,13 +6,12 @@ function language(value) {
     return structuredClone({fr, en}[value])
 }
 
-const currentLanguage = language(SettingsService.ui.language)
-const translations = Vue.reactive(currentLanguage)
+const translations = Vue.reactive(language(SettingsService.ui.language))
 
 Vue.watch(
   () => SettingsService.ui.language,
   (value) => {
-    Object.assign(currentLanguage, language(value))
+    Object.assign(translations, language(value))
   }
 )
 
