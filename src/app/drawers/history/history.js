9cf8a8b4a0a3985d05349fe34fdc8c7c28a8d7b5
James Hoffman <james-hoffman@live.ca>
2026-05-26 20:41:22 -0400

    ui experiments

--- src/app/drawers/history/history.js
+++ src/app/drawers/history/history.js
@@ -25,25 +25,34 @@ export default {
   },
 
   template: `
-    <drawer class="_drawers_history_history" :title="t.drawers.history.title">
-      <component is="style">
-        ._drawers_history_history {
-          h1 {
-            margin: 0;
+    <div style="overflow-y: scroll; height: 100%;">
+      <drawer class="_drawers_history_history" :title="t.drawers.history.title">
+        <component is="style">
+          ._drawers_history_history {
+            padding-bottom: 0 !important;
+
+            h1 {
+              margin: 0;
+            }
           }
-        }
-      </component>
+        </component>
 
-      <p v-if="requests.length == 0" style="font-style: italic;">{{ t.drawers.history.empty }}</p>
+        <p v-if="requests.length == 0" style="font-style: italic;">{{ t.drawers.history.empty }}</p>
+      </drawer>
 
-      <v-list v-else>
+      <v-list v-if="requests.length > 0">
 
-        <v-list-item 
-          v-for="request of requests"
-          style="padding: 4px 0;"
-        >
-          <v-list-item-title style="display: flex; gap: 0.5rem; align-items: end;">
-            <span class="text-label-large">{{ request.method }}</span>
+        <template v-for="(request, index) of requests">
+          <v-list-item link>
+            <v-list-item-title style="display: flex; gap: 0.5rem; align-items: baseline;">
+              <span class="text-label-large">{{ request.method }}</span>
+              <span>{{ request.path }}</span>
+            </v-list-item-title>
+
+            <v-list-item-title></v-list-item-title>
+
+            <v-list-item-subtitle>{{ request.host }}</v-list-item-subtitle>
+
             <v-chip 
               v-if="request.response"
               :append-icon="request.response.redirected ? 'mdi-chevron-double-right' : null"
@@ -54,13 +63,12 @@ export default {
               size="small"
               density="comfortable"
             />
-          </v-list-item-title>
-          <v-list-item-title>{{ request.path }}</v-list-item-title>
-          <v-list-item-subtitle>{{ request.host }}</v-list-item-subtitle>
-          <v-divider no-inset></v-divider>
-        </v-list-item>
+          </v-list-item>
+
+          <v-divider v-if="index != requests.length - 1"></v-divider>
+        </template>
 
       </v-list>
-    </drawer>
+    </div>
   `
 }
