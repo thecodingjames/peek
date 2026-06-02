465a3d78b91500a34e07b0cf72017a31301e6a35
James Hoffman <james-hoffman@live.ca>
2026-06-02 16:06:04 -0400

    wip editable key-value ui/ux

--- src/app/http/request-details/editable-key-value.js
+++ src/app/http/request-details/editable-key-value.js
@@ -8,6 +8,21 @@ export default {
     'modelValue',
   ],
 
+  data() {
+    return {
+      items: Object.entries({ ...this.modelValue }).reduce( (acc, [key, value]) => {
+        return [
+          ...acc,
+          {
+            key,
+            value,
+            enabled: true,
+          },
+        ]
+      }, [])
+    }
+  },
+
   methods: {
     test() {
       this.$emit('update:modelValue', { a: 1})
@@ -15,14 +30,40 @@ export default {
   },
 
   template: `
-    <v-table striped="even" style="user-select: text;">
+    <v-table style="user-select: text;">
       <tbody>
         <tr
-          v-for="(value, key) in modelValue"
-          :key
+          v-for="item in items"
+          :key="item.key"
+
+          :style="{ opacity: item.enabled ? 1 : 0.5, }"
         >
-          <td style="user-select: text; cursor: text; white-space: nowrap;">{{ key }}</td>
-          <td style="user-select: text; cursor: text;">{{ value }}</td>
+          <td>
+            <v-icon icon="mdi-drag-vertical" />
+          </td>
+
+          <td>
+            <v-checkbox
+              v-model="item.enabled"
+              hide-details
+            />
+          </td>
+
+          <td>
+            <v-text-field
+              v-model="item.key"
+              density="compact"
+              hide-details
+            />
+          </td>
+
+          <td>
+            <v-text-field
+              v-model="item.value"
+              density="compact"
+              hide-details
+            />
+          </td>
         </tr>
       </tbody>
     </v-table>
