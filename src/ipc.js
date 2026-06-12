4db06c8b26bc0a8c3bbc464318cb90018d7edac8
James Hoffman <james-hoffman@live.ca>
2026-06-12 15:01:38 -0400

    clean console log

--- src/ipc.js
+++ src/ipc.js
@@ -4,7 +4,6 @@ function registerHttp() {
   ipcMain.handle('http', async (_, { url, ...options }) => {
 
     try {
-      console.log(options)
       const response = await fetch(url, options)
 
       const blob = await response.text()
